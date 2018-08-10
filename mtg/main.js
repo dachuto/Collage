0cc9a752a4aaf159c93354dc3c5fc225895b3348
dachuto <dachuto@gazeta.pl>
2018-08-10 08:56:11 +0200

    frontend changes for queries

--- mtg/main.js
+++ mtg/main.js
@@ -1,12 +1,8 @@
 "use strict";
 
 class cards_data_source {
-	constructor(id_to_name_object) {
-		this.id_to_name_object = id_to_name_object;
-	}
-
 	id_to_name(multiverse_id) {
-		return this.id_to_name_object[multiverse_id];
+		return "TODO";
 	}
 
 	scryfall_link(name) {
@@ -19,6 +15,7 @@ class cards_data_source {
 	}
 
 	image_url(multiverse_id) {
+		// return "https://api.scryfall.com/cards/multiverse/" + multiverse_id + "?format=image";
 		return "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" + multiverse_id;
 	}
 }
@@ -69,11 +66,288 @@ function failed_json(jqXHR, textStatus, errorThrown) {
 	console.log(errorThrown);
 }
 
+function DFS(graph, start, visit_pre, visit_post) {
+	let encountered = new Set();
+	let processed = new Set();
+	let stack = [];
+
+	let encounter = v => {
+		if (!encountered.has(v)) {
+			encountered.add(v);
+			visit_pre(v);
+			if (!processed.has(v)) {
+				stack.push(v);
+			}
+		}
+	};
+
+	if (start !== undefined) {
+		encounter(start);
+	}
+
+	for (const [i, unused] of graph) {
+		encounter(i);
+
+		while (stack.length != 0) {
+			const vertex = stack[stack.length - 1];
+			if (processed.has(vertex)) {
+				stack.pop();
+				visit_post(vertex);
+				continue;
+			}
+			processed.add(vertex);
+			let edges = graph.get(vertex);
+			for (const destination of edges) {
+				encounter(destination);
+			}
+		}
+	}
+}
+
+function topological_order(graph) {
+	let post_order = [];
+	DFS(graph, undefined, ()=>{}, (v) => {
+		post_order.push(v);
+	});
+	return post_order.reverse();
+}
+
+class tag {
+	constructor(element, name) {
+		this.element = element;
+		this.name = name;
+		this.state = "unset";
+		this.state_implicit = null;
+		this.timestamp = 0;
+		this.timestamp_implicit = 0;
+	}
+
+	explicit() {
+		switch (this.state) {
+		case "in":
+		case "out":
+			return true;
+		}
+		return false;
+	}
+
+	max_timestamp() {
+		return Math.max(this.timestamp, this.timestamp_implicit);
+	}
+
+	reset_implicit() {
+		if (this.state_implicit !== null) {
+			console.log("HAUDHSAD " + this.state_implicit);
+		}
+		this.state_implicit = null;
+		this.timestamp_implicit = 0;
+	}
+
+	imply(destination) {
+		if (destination.explicit()) {
+			return;
+		}
+
+		if (destination.max_timestamp() > this.max_timestamp()) {
+			return;
+		}
+
+		if (this.state == "unset" && this.state_implicit === null) {
+			return;
+		}
+
+		if (this.state == "in" || this.state_implicit == "in") {
+			destination.state_implicit = "in";
+			destination.timestamp_implicit = this.max_timestamp();
+		}
+
+		if (this.state == "out" || this.state_implicit == "out") {
+			destination.state_implicit = "out";
+			destination.timestamp_implicit = this.max_timestamp();
+		}
+	}
+
+	rotate(current_timestamp) {
+		const next_state = state => {
+			switch (state) {
+			case "unset":
+				return "in";
+			case "in":
+				return "out";
+			case "out":
+				return "unset";
+			}
+		};
+		this.state = next_state(this.state);
+		if (this.explicit()) {
+			this.timestamp = current_timestamp;
+		} else {
+			this.timestamp = 0;
+		}
+		this.update_element();
+	}
+
+	css_class() {
+		if (this.state_implicit === null) {
+			return "card_tag_" + this.state;
+		} else {
+			return "card_tag_" + this.state_implicit + "_implicit";
+		}
+	}
+
+	update_element() {
+		const inner = this.element.firstChild;
+		inner.className ="";
+		inner.classList.add("card_tag_inner", this.css_class());
+	}
+}
+
+class page_data {
+	constructor() {
+		this.backend_url = new URL("http://localhost:8080");
+		this.images_info = {loaded: 0, total: 0};
+		this.message_box = document.querySelector("#message_box");
+		this.message_box_timer = null;
+		this.source = new cards_data_source();
+		this.search_button = document.getElementById("search_button");
+		this.search_input = document.getElementById("search_input");
+		this.search_input.value = window.location.search;
+		this.search_button.onclick = () => {
+			// console.log(encodeURIComponent(SI.value));
+			// data.start_async_loading();
+			window.history.pushState(null, "", "http://localhost:1234/mtg_results.html?set=BBD");
+			// window.history.replaceState(null, "", "http://localhost:1234/mtg_results.html?set=BBD");
+		};
+	}
+
+	start_async_loading() {
+		console.log(location);
+		console.log(decodeURIComponent(location.search));
+
+		let info_url = new URL("/info", this.backend_url);
+		let query_url = new URL("/query", this.backend_url);
+		query_url.search = window.location.search;
+
+		$.when(
+			JSON_request(info_url.href),
+			JSON_request(query_url.href)
+		).then(
+			this.async_complete.bind(this),
+			null
+		);
+	}
+
+	async_complete(info, query) {
+		this.graph = new Map();
+		this.tags = new Map();
+		this.tags_timestamp = 0;
+		for (const [key, value] of Object.entries(info[0])) {
+			const tag_id = Number(key);
+			const element = this.card_tag_html(tag_id, value);
+			$("#card_tags_container").append(element);
+			this.graph.set(tag_id, []);
+			const t = new tag(element, name);
+			this.tags.set(tag_id, t);
+			t.update_element();
+		}
+		//TODO: this graph comes from info server !!!
+		this.graph.set(1, [2]);
+		this.graph.set(100, [102, 104]);
+		this.graph.set(102, [103, 104]);
+		console.log(this.graph);
+		this.graph_topological_order = topological_order(this.graph);
+
+		DOM_append(images_grid(query[0], this.source));
+		this.start_lazy_images_loading();
+	}
+
+	card_tag_html(id, name, state) {
+		let inner = document.createElement("div");
+		inner.classList.add(state);
+		inner.setAttribute("data-id", id);
+		inner.innerHTML = name;
+
+		let tag = document.createElement("div");
+		tag.className = "card_tag_item";
+		tag.onclick = event => {
+			// console.log(event);
+			++this.tags_timestamp;
+			this.tags.get(id).rotate(this.tags_timestamp);
+
+			for (const [id, t] of this.tags) {
+				t.reset_implicit();
+			}
+
+			for (const x of this.graph_topological_order) {
+				const source = this.tags.get(x);
+				for (const destination_id of this.graph.get(x)) {
+					const destination = this.tags.get(destination_id);
+					source.imply(destination);
+				}
+			}
+
+			for (const [id, t] of this.tags) {
+				t.update_element();
+			}
+		};
+		tag.appendChild(inner);
+		return tag;
+	}
+
+	start_lazy_images_loading() {
+		let options = {
+			root: null,
+			rootMargin: '150%',
+			threshold: [0.0]
+		};
+
+		let observer = new IntersectionObserver((entries, observer) => {
+			for (const e of entries) {
+				if (e.isIntersecting) {
+					image_load(e.target);
+					++this.images_info.loaded;
+					this.update_message_box();
+					observer.unobserve(e.target);
+				}
+			}
+		}, options);
+
+		for (const t of document.querySelectorAll(".image_inactive")) {
+			++this.images_info.total;
+			observer.observe(t);
+		}
+	}
+
+	update_message_box() {
+		$(this.message_box)
+			.addClass("visible")
+			.text("Images " + this.images_info.loaded + "/" + this.images_info.total);
+		if (this.message_box_timer != null) {
+			window.clearTimeout(this.message_box_timer);
+		}
+		this.message_box_timer = window.setTimeout(() => {
+			$(this.message_box).removeClass("visible");
+		}, 5000);
+	}
+}
+
 function DOM_ready() {
 	$.when(
 		JSON_request("mtg/article.json"),
-		JSON_request("mtg/id_to_name.json"))
-	.then(all_data_is_here, null);
+		JSON_request("mtg/name_to_tag.json"),
+		JSON_request("mtg/tags.json")
+	).then(all_data_is_here, null);
+}
+
+function DOM_ready_results() {
+	document.addEventListener("keyup", event => {
+		console.log(event);
+		// if (event.key !== "Enter") return; // Use `.key` instead.
+		// document.querySelector("#linkadd").click(); // Things you want to do.
+		event.preventDefault(); // No need to `return false;`.
+	});
+	let data = new page_data();
+	data.start_async_loading();
 }
 
 function DOM_append(element) {
@@ -116,6 +390,34 @@ function articlesJSON_groupped(data, source) {
 	return group_by_keys_set.get(temp);
 }
 
+function image_not_loaded_html(url) {
+	let a = document.createElement("div");
+	a.classList.add("aspect-ratio-box");
+	let b = document.createElement("div");
+	b.classList.add("aspect-ratio-box-inside", "image_inactive");
+	b.setAttribute("data-src", url);
+	a.appendChild(b);
+	return a;
+}
+
+function image_load(e) {
+	let image = document.createElement("img");
+	image.classList.add("aspect-ratio-box-inside", "preview");
+	image.src = e.getAttribute("data-src");
+	e.parentNode.replaceChild(image, e);
+}
+
+function images_grid(keys, source) {
+	let images_grid = $("<div/>", {
+		class: "images_grid",
+	});
+
+	for (const k of keys) {
+		images_grid.append(image_not_loaded_html(source.image_url(k)));
+	}
+	return images_grid;
+}
+
 function item_html(keys, htmls, item_number, source) {
 	let item = $("<div/>", {
 		class: "article_grid"
@@ -128,18 +430,6 @@ function item_html(keys, htmls, item_number, source) {
 	let sticker = $("<div/>", {
 		class: "top_sticky",
 	});
-
-	let images_grid = $("<div/>", {
-		class: "images_grid",
-	});
-
-	for (const k of keys) {
-		let image = $("<img/>", {
-			class: "preview",
-			src: source.image_url(k)
-		});
-		images_grid.append(image);
-	}
 
 	let segments = $("<div/>", {
 		class: "ui raised stacked segments",
@@ -155,7 +445,7 @@ function item_html(keys, htmls, item_number, source) {
 
 	let text_column = $("<div/>");
 
-	sticker.append(images_grid);
+	sticker.append(images_grid(keys, source));
 	images_column.append(sticker);
 	item.append(images_column);
 	text_column.append(segments);
@@ -177,11 +467,12 @@ function all_articles_html(keys_sets_to_articles, source) {
 	return one_big;
 }
 
-function all_data_is_here(article, id_to_name) {
+function all_data_is_here(article, name_to_tags, tags) {
 	// console.log(article[0]);
 	// console.log(id_to_name[0]);
-	let source = new cards_data_source(id_to_name[0]);
+	console.log(name_to_tags[0]);
+	console.log(tags[0]);
+	let source = new cards_data_source();
 	let groups = articlesJSON_groupped(article[0], source);
-	console.log(groups);
 	DOM_append(all_articles_html(groups, source));
 }
--- mtg/prepare_data.py
+++ mtg/prepare_data.py
@@ -1,18 +1,17 @@
 import argparse
 import json
 
-def extract_info_2(data):
-	cards = data['cards']
-	for c in cards:
-		try:
-			name = c['name']
-		except KeyError:
-			print('No name!', c)
-		
-		try:
-			c['multiverseid']
-		except KeyError:
-			print('No multiverseid !', c)
+class filter_merge:
+	def __init__(self):
+		self.merged = dict()
+
+	def extract(self, data):
+		for c in data["DOM"]["cards"]:
+			self.merged[c["name"]] = [1]
+
+	def done(self):
+		print(json.dumps(self.merged, indent="\t", separators=(",", ":"), sort_keys=True))
+
 
 class invert_map:
 	def __init__(self):
@@ -44,12 +43,15 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Prepare data from mtgjson.com files. Take multiple json files and output one.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('input_files', help='in json format', nargs='*')
 	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('--filter', action='store_true', help='...')
 	group.add_argument('--invert', action='store_true', help='invert top level maps {key : value} => {value : [keys]}')
 	group.add_argument('--merge', action='store_true', help='merge top level maps')
 
 	args = parser.parse_args()
 
 	merger = None
+	if args.filter:
+		merger = filter_merge()
 	if args.invert:
 		merger = invert_map()
 	if args.merge:
