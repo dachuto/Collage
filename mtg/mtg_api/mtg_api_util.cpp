71c133f90d021218a201bcc207fcc05ce4776241
dachuto <dachuto@gazeta.pl>
2019-07-03 18:18:48 +0200

    minor

---
+++ mtg/mtg_api/mtg_api_util.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "json.hpp"
+
+int main() {
+	auto database = mtg_api::read({"./AllCards.json", "./name_to_tags.json", "./AllSets.json", "./tags.json"});
+
+	for (auto const &kv: database.unique_cards) {
+		std::cout << kv.first << "\n";
+	}
+
+	return 0;
+}
