f4a48d7c600332f9ecf68756f952a2f4e07c6a7a
dachuto <dachuto@gazeta.pl>
2019-09-06 19:16:40 +0200

    new deck

--- mtg/main.js
+++ mtg/main.js
@@ -212,6 +212,7 @@ class page_data {
 
 		this.commander_deck_param = "deck";
 		this.commander_decks = new Map();
+		this.commander_decks.set("Artifacts", "commander_artifacts.json");
 		this.commander_decks.set("estrid", "commander_estrid.json");
 		this.commander_decks.set("Grenzo", "commander_grenzo.json");
 		this.commander_decks.set("roon", "commander_roon.json");
