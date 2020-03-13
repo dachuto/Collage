a5b2d859940e0ee3a54fbe01c91b94f63478541f
dachuto <dachuto@gazeta.pl>
2020-03-13 14:27:46 +0100

    fog changes

--- mtg/main.js
+++ mtg/main.js
@@ -231,6 +231,8 @@ class page_data {
 		this.commander_decks.set("takeover", "commander_takeover.json");
 		this.commander_decks.set("Teysa", "commander_teysa.json");
 		this.commander_decks.set("Wort", "commander_wort.json");
+		this.commander_decks.set("--FOG", "commander_fog.json");
+		this.commander_decks.set("--Removals", "commander_removal.json");
 		this.commander_decks.set("CUBE", "cube.json");
 		this.commander_decks.set("WANTS", "wants.json");
 		this.commander_default_deck = this.commander_decks.keys().next().value;
